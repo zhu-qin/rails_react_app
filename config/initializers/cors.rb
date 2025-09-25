2149da51848785d9811bf60a17dc11d139888bd2
Qin Qin Zhu <qqinzhu@gmail.com>
2025-09-25 18:46:04 -0400

    change routes to use /api/v1

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    origins "http://127.0.0.1:5173"
 
     resource "*",
       headers: :any,
