abe3740a69c33173a919fe77379436f86c2c6f49
Qin Qin Zhu <qqinzhu@gmail.com>
2025-09-25 20:43:37 -0400

    Added the Vite React client app

---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+export const API_URL = 
+    process.env.NODE_ENV === "test" 
+        ? "http:://mocked-api.url"
+        : import.meta.env.VITE_API_URL;
+        // : "http://localhost:3000/api/v1/posts"
