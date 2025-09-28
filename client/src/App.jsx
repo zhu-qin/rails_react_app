b9c1b7a89bff54d209dd6ab4bfb5885c35b500c0
Qin Qin Zhu <qqinzhu@gmail.com>
2025-09-28 13:49:52 -0400

    Add routing and post show page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
-import PostsList from './features/posts/PostsList'
+import './App.css';
+// import PostsList from './features/posts/PostsList';
+import {BrowserRouter as Router} from "react-router-dom";
+import NavBar from "./components/NavBar.jsx"
+import AppRoutes from "./components/AppRoutes.jsx";
 
 function App() {
 
 
   return (
-    <>
+    <Router>
       <div className="app">
         <h1>React on Rails Blog</h1>
         <p>Find this application layout in client/src/App.jsx</p>
-        <PostsList/>
+        <NavBar/>
+        <AppRoutes/>
       </div>
-    </>
+    </Router>
   )
 }
 
